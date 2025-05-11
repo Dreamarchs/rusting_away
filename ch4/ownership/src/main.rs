882ebce8c864a7de17ab5bad60421da564b83398
Deven West <devenw5@gmail.com>
2025-05-11 18:03:22 -0400

    Mac: more ownership examples

--- ch4/ownership/src/main.rs
+++ ch4/ownership/src/main.rs
@@ -7,9 +7,15 @@ fn main() {
     */
     println!("{s}");
 
+    takes_ownership(s); //since we pass s to the function it's moved, and the function now owns it
+
     let x = 5;
     let y = x;
     //The above y variable automatically creates a copy of the value that x has since it's a simple i32 number.
+
+    makes_copy(y);
+    println!("{x} and {y}"); //we can still call x + y because even though we passed them to a function, simple structs that are only on the stack
+    // are easily copied so Rust handles them that way. integers, floats, bools and tuples that only contain the simple types listed can also implement the Copy trait
 
     let s1 = String::from("hello");
     let s2 = s1;
@@ -18,4 +24,13 @@ fn main() {
 
     let s3 = s2.clone();
     //the above create an real copy completely free of any binding of s2 it would have without .clone()
+}
+
+
+fn takes_ownership(some_string: String){
+    println!("{some_string}");
+}
+
+fn  makes_copy(some_integer: i32) {
+    println!("{some_integer}");
 }
